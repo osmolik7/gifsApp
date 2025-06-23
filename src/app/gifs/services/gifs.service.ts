a8bf7047a95dfe0a5adb9201680f6d845062828d
alejandro <camacho044@gmail.com>
2025-06-23 16:22:31 -0500

    search completo con localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,17 @@
 import { HttpClient } from '@angular/common/http';
-import { computed, inject, Injectable, signal } from '@angular/core';
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
 import { environment } from '@environments/environment';
 import type { GiphyResponse } from '../interfaces/giphy.interfaces';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
-import { map, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
+
+
+const loadFromLocalStorage = () => {
+    const gifsFromLocalStorage = localStorage.getItem('gifs') ?? '{}';
+    const gifs = JSON.parse(gifsFromLocalStorage);
+    return gifs;
+}
 
 @Injectable({providedIn: 'root'})
 export class GifService {
@@ -13,12 +20,18 @@ export class GifService {
     trendingGifs = signal<Gif[]>([]);
     trendingGifsLoading = signal(true);
 
-    searchHistory = signal<Record<string, Gif[]>>({});
+    searchHistory = signal<Record<string, Gif[]>>(loadFromLocalStorage());
     searchHistoryKeys = computed(() => Object.keys(this.searchHistory()));
 
     constructor(){
         this.loadTrendigGifs();
     }
+
+    saveGifsToLocalStorage = effect(() => {
+        const historyString = JSON.stringify(this.searchHistory());
+        localStorage.setItem('gifs', historyString);
+    });
+
 
     loadTrendigGifs(){
         this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
@@ -33,7 +46,7 @@ export class GifService {
         });
     }
 
-    searchGifs(query: string){
+    searchGifs(query: string): Observable<Gif[]>{
         return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
             params: {
                 api_key: environment.apiKey,
@@ -51,5 +64,9 @@ export class GifService {
                 }))
             })
         );
+    }
+
+    getHistoryGifs(query:string):Gif[]{
+        return this.searchHistory()[query] ?? [];
     }
 }
